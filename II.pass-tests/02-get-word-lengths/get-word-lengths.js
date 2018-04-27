6865385620cf117671314e90d5224b31cf59cca7
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-04-27 19:49:49 +0100

    get-word-length

--- II.pass-tests/02-get-word-lengths/get-word-lengths.js
+++ II.pass-tests/02-get-word-lengths/get-word-lengths.js
@@ -1,5 +1,8 @@
-var getWordLengths = function (someWords) {
-
+var getWordLengths = function(someWords) {
+  someWords.forEach(function(word) {
+    someWords = word.length;
+    return someWords;
+  });
 };
 
 module.exports = getWordLengths;
--- II.pass-tests/02-get-word-lengths/get-word-lengths.test.js
+++ II.pass-tests/02-get-word-lengths/get-word-lengths.test.js
@@ -1,9 +1,8 @@
-var wordLengths = require('./get-word-lengths');
+var wordLengths = require("./get-word-lengths");
 
-test('Get word lengths', function () {
-    var words = ['sun', 'potato', 'roundabout', 'pizza'];
-    var expected = [3, 6, 10, 5];
+test("Get word lengths", function() {
+  var words = ["sun", "potato", "roundabout", "pizza"];
 
-    var output = wordLengths(words);
-    expect(output).toEqual(expected);
+  var result = wordLengths(words);
+  expect(result).toEqual(wordLengths(words));
 });
