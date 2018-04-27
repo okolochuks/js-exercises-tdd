78e2ee3561a23e58f861e984797ccf29bb274932
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-04-27 15:20:34 +0100

    numbers-greater-than-10-and-largest-number-tests

--- I.write-tests/06-largest-number/largest-number.test.js
+++ I.write-tests/06-largest-number/largest-number.test.js
@@ -1,3 +1,13 @@
+var getLargestNumber = require("./largest-number");
+test("Get largest number", function() {
+  // Arrange
+  var array = [3, 21, 88, 4, 36];
+  // Act
+  var result = Math.max(array);
+  // Assert
+  expect(result).toEqual(Math.max(array));
+});
+
 // example
 // input: [3, 21, 88, 4, 36];
 // expected: 88;
