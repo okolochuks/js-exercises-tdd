53335a5e8ebd6d35b8d7b42c44540a4dd0026554
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-04-27 16:39:18 +0100

    get-even-numbers-test

--- I.write-tests/07-get-even-numbers/get-even-numbers.test.js
+++ I.write-tests/07-get-even-numbers/get-even-numbers.test.js
@@ -1,3 +1,13 @@
+var getEvenNumbers = require("./get-even-numbers");
+test("get even numbers", function() {
+  // Arrange
+  var arr = [22, 13, 73, 82, 4];
+  // Act
+  var result = arr % 2 === 0;
+  // Assert
+  expect(result).toEqual(arr % 2 === 0);
+});
+
 // example
 // input: [22, 13, 73, 82, 4];
 // expected: [22, 82, 4];
