c45e923efb482caaa3ab03bcfef7b8150bd6deea
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-04-27 20:04:25 +0100

    numbers-greater-than-ten-test

--- I.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
+++ I.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
@@ -1,9 +1,13 @@
-var largerThanTen = require('./numbersGreaterThan10');
+var largerThanTen = require("./numbersGreaterThan10");
 
-test('Get numbers greater than 10', function () {
-
+test("Get numbers greater than 10", function() {
+  // Arrange
+  var array = [4, 10, 32, 9, 21];
+  // Act
+  var result = array > 10;
+  // Assert
+  expect(result).toEqual(array > 10);
 });
-
 
 // input: [4, 10, 32, 9, 21];
 // expected output: [32, 21];
