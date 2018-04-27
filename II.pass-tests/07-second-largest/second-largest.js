84e74b2c474b91bb5648909c0a0d4f95c6dfec93
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-04-27 22:56:23 +0100

    second-largest-test

---
+++ II.pass-tests/07-second-largest/second-largest.js
@@ -0,0 +1,6 @@
+function secondLargest(numbers) {
+  var secondLargest;
+  for (var i = 0; i < numbers.length - 1; i++) {
+    if (numbers[i] < numbers[i - 1]) secondLargest = number[i];
+  }
+}
--- II.pass-tests/07-second-largest/second-largest.test.js
+++ II.pass-tests/07-second-largest/second-largest.test.js
@@ -1,9 +1,9 @@
-var secondLargest = require('./second-largest');
+var secondLargest = require("./second-largest");
 
-test('Second largest', function () {
-    var numbers = [2, 0, 23, 0, 57, 1];
+test("Second largest", function() {
+  var numbers = [2, 0, 23, 0, 57, 1];
 
-    var output = secondLargest(numbers);
+  var result = numbers[2];
 
-    expect(output).toEqual(2);
+  expect(result).toEqual(numbers[2]);
 });
