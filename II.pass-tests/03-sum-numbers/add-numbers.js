a2002984bdeb2f00310aaa8d1571a5a5f0097096
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-04-27 23:44:48 +0100

    add-numbers

--- II.pass-tests/03-sum-numbers/add-numbers.js
+++ II.pass-tests/03-sum-numbers/add-numbers.js
@@ -1,5 +1,5 @@
 function addNumbers(numbers) {
-  for (var i = 1; i < numbers.length - 1; i++) {
+  for (var i = 1; i === numbers.length - 1; i++) {
     return numbers[i + i];
   }
   return numbers[i + i];
